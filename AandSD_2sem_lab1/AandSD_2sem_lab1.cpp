a929a3138b4f8914137a026783ee09043f0824c4
Admin <Admin@LAPTOP-U4DN8FG5>
2022-04-11 19:14:19 +0400

    Добавлено меню. Остались графики.

--- AandSD_2sem_lab1/AandSD_2sem_lab1.cpp
+++ AandSD_2sem_lab1/AandSD_2sem_lab1.cpp
@@ -1,18 +1,92 @@
 ﻿#include <iostream>
 #include <conio.h>
 #include "Map.h"
+
 using namespace std;
 
-int main()
+int GetKey()
 {
-    Map m;
-    m.Insert(5, "dac");
-    m.Insert(3, "dsac");
-    m.Insert(7, "dsac");
-    m.Insert(12, "sdsac");
-    m.Insert(4, "ssdddsac");
-    m.Insert(11, "ssdddsac");
-    m.Print();
+    int Key = _getch();
+    if ((Key == 0) || (Key == 224)) Key = _getch();
+    return Key;
+}
 
+void menu(Map& m) {
+
+    int key;
+    string data;
+    int Repeat = 1;
+    cout << "[0]: Insert element\n[1]: Erase element\n[2]: Find element\n[3]: Exit\n" << "\n";
+    cout << "Your map: " << "\n";
+    m.Print();
+    while (Repeat != 0)
+    {
+        int k = GetKey();
+        switch (k)
+        {
+        case 48:
+            system("cls");
+            {
+                cout << "Enter the key:";
+                cin >> key;
+                cout << "Enter the data:";
+                cin >> data;
+                try {
+                    m.Insert(key, data);
+                }
+                catch (const char* err)
+                {
+                    cerr << err;
+                }
+                menu(m);
+            }
+            break;
+        case 49:
+            system("cls");
+            cout << "Enter the key:";
+            cin >> key;
+            try {
+                m.Erase(key);
+            }
+            catch (const char* err)
+            {
+                cout << err;
+                system("pause");
+                break;
+            }
+            menu(m);
+            break;
+        case 50:
+            system("cls");
+            cout << "Enter the key:";
+            cin >> key;
+            try {
+                cout << "Data with this key:" << m.FindElement(key) << "\n";
+                system("pause");
+            }
+            catch (const char* err)
+            {
+                std::cout << err;
+                system("pause");
+                break;
+            }
+            menu(m);
+            break;
+        case 51:
+            system("cls");
+            m.~Map();
+            cout << "Thank you! ^_^" << "\n";
+            exit(0);
+            break;
+        default: cout << "Select [0-4]" << "\n";
+        }
+    }
+}
+
+int main(){
+    Map m;
+    cout << "Hello, user!^_^" << "\n";
+    menu(m);
     return 0;
 }
+
